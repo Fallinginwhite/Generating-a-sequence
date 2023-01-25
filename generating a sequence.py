1cbc5a6029f29490972dc3b10b0a1a1b2b065a57
Fallinginwhite <92825691+Fallinginwhite@users.noreply.github.com>
2023-01-25 20:20:35 +0000

    Add files via upload

---
+++ generating a sequence.py
@@ -0,0 +1,13 @@
+initial_seed = 3
+value = 3
+print(value)
+
+
+    
+while value != initial_seed:
+    value = (value * 9)
+    value = value + 5
+    value = value % 8
+    print(value)
+
+        
